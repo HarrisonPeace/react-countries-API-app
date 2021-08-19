28a9aa110baf0e3f5943b87c7c36317f6c32ae56
Harrison Peace <harrison@apwines.com>
2021-08-19 11:29:10 +1000

    Added code comments

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,35 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 
+// Component Imports
 import PerPage from "./custom-select/items-per-page";
 import SearchFrom from "./search-form";
 import Pages from "./pagination";
 import FilterByRegion from "./custom-select/filter-by-region";
+import Loading from "../loading";
 
+// helper Imports
 import filterResults from "../helpers/filter-results";
 import formatResults from "../helpers/format-results";
-import Loading from "../loading";
 
 const queryString = require("query-string");
 
-//Index Component
 const Index = ({ data }) => {
   //Create location reference
   let location = useLocation();
 
-  const query = queryString.parse(location.search);
-  query.perPage = parseInt(query.perPage || 12);
-  query.page = parseInt(query.page || 1);
-  let { search, region, perPage, page } = query;
+  const query = queryString.parse(location.search); //get query string from URL turn it into an object
+  query.perPage = parseInt(query.perPage || 12); //turn perPage into number or 12
+  query.page = parseInt(query.page || 1); //turn "current" page into number or "page" 1
+  let { search, region, perPage, page } = query; //create variables for each query parameter
 
   const [totalPages, setTotalPages] = useState(1);
   const [formattedData, setFormattedData] = useState(null);
 
+  /**
+   * @description filter and format the data to be show on the page by passing it through both the filterResults and formatResults helpers (see individual documents for how these work)
+   * @Return sets FormattedData state
+   */
   useEffect(() => {
     let filteredData = filterResults(data, search, region);
     setFormattedData(formatResults(filteredData, setTotalPages, perPage));
   }, [data, region, perPage, search]);
 
+  //if useEffect has not worked yet (initial mount) return loading
   if (formattedData === null) {
     return <Loading />;
   } else {
@@ -42,37 +48,38 @@ const Index = ({ data }) => {
             <FilterByRegion query={query} />
           </div>
         </div>
-        {
-          formattedData.length === 0 ?
+        {formattedData.length === 0 ? (
           <div className="not-found-error-container">
-        <h2>Sorry no results where found</h2>
-        <p>Please try again.</p>
-      </div> :
-      <>
-      <div className="countries-container">
-          {formattedData[page - 1].map((country) => (
-            <div className="country-container" key={country.name}>
-              <Link to={`/country/${country.alpha3Code.toLowerCase()}`}>
-                <img src={country.flag} alt={`${country.name}'s Flag`} />
-                <div className="text-container">
-                  <h1>{country.name}</h1>
-                  <span>
-                    <span>Population:</span> {country.population}
-                  </span>
-                  <span>
-                    <span>Region:</span> {country.region}
-                  </span>
-                  <span>
-                    <span>Capital:</span> {country.capital}
-                  </span>
+            <h2>Sorry no results where found</h2>
+            <p>Please try again.</p>
+          </div>
+        ) : (
+          <>
+            <div className="countries-container">
+              {/* -- Map over all countries in the formatted results array and create a JSX element  */}
+              {formattedData[page - 1].map((country) => (
+                <div className="country-container" key={country.name}>
+                  <Link to={`/country/${country.alpha3Code.toLowerCase()}`}>
+                    <img src={country.flag} alt={`${country.name}'s Flag`} />
+                    <div className="text-container">
+                      <h1>{country.name}</h1>
+                      <span>
+                        <span>Population:</span> {country.population}
+                      </span>
+                      <span>
+                        <span>Region:</span> {country.region}
+                      </span>
+                      <span>
+                        <span>Capital:</span> {country.capital}
+                      </span>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
+              ))}
             </div>
-          ))}
-        </div>
-        <Pages query={query} totalPages={totalPages} />
-        </>
-        }
+            <Pages query={query} totalPages={totalPages} />
+          </>
+        )}
       </main>
     );
   }
--- src/components/home/pagination.js
+++ src/components/home/pagination.js
@@ -7,92 +7,45 @@ function Pages({ query, totalPages }) {
   //Create history reference
   let history = useHistory();
 
+  //Create "current" page variable from query
   let { page } = query;
 
+  //set state
   const [pageButtonsState, setPageButtonsState] = useState(null);
   const [svgButtonsState, setSvgButtonsState] = useState(null);
 
-  const setSVGButtons = useCallback((svgArray, pageButtonArray) => {
-    if(totalPages < 5) {
-      svgArray[0].className = "disable-button";
-      svgArray[1].className = "disable-button";
-    } else if (pageButtonArray[0].page === 1) {
-      svgArray[0].className = "disable-button";
-      svgArray[1].className = "none";
-    } else if (pageButtonArray[3].page !== "...") {
-      svgArray[0].className = "none";
-      svgArray[1].className = "disable-button";
-    } else {
-      svgArray[0].className = "";
-      svgArray[1].className = "";
-    }
-    return svgArray
-  }, [totalPages]);
-
-
-  const pageContainerClick = (e) => {
-    let pageButtons = JSON.parse(JSON.stringify(pageButtonsState));
-    let svgButtons = JSON.parse(JSON.stringify(svgButtonsState));
-    let location = parseInt(e.target.innerHTML);
-
-    const setPageButtons = (direction) => {
-      pageButtons[4].page = totalPages;
-      pageButtons[3].page = "...";
-      pageButtons[3].className = "no-hover";
-      for (let i = 0; i < 3; i++) {
-        direction === "backwards" ?
-        pageButtons[i].page = pageButtons[i].page - 1
-        : pageButtons[i].page = pageButtons[i].page + 1
-        if (pageButtons[i].page === page) {
-          pageButtons[i].className = "current-page";
-        } else {
-          pageButtons[i].className = "";
-        }
+  /**
+   * @description sets weather the arrow SVG(s) should be disabled/hidden or shown depending on the current page numbers shown
+   * @useCallBack is used so that the component is not re-rendered on every mount causing an infinite loop, only updates when total Pages changes
+   * @param svgArray = copy of the svgButtonsState array
+   * @param pageButtonArray = copy of the pageButtonsState array
+   * @Return svgArray
+   */
+  const setSVGButtons = useCallback(
+    (svgArray, pageButtonArray) => {
+      if (totalPages < 5) {
+        svgArray[0].className = "disable-button";
+        svgArray[1].className = "disable-button";
+      } else if (pageButtonArray[0].page === 1) {
+        svgArray[0].className = "disable-button";
+        svgArray[1].className = "none";
+      } else if (pageButtonArray[3].page !== "...") {
+        svgArray[0].className = "none";
+        svgArray[1].className = "disable-button";
+      } else {
+        svgArray[0].className = "";
+        svgArray[1].className = "";
       }
-    }
+      return svgArray;
+    },
+    [totalPages]
+  );
 
-    if (
-      e.target.nodeName === "BUTTON" &&
-      e.target.innerHTML !== "..." &&
-      location !== page
-    ) {
-      query.page = location;
-      history.push({
-        pathname: "/",
-        search: queryString.stringify(query),
-      });
-    } else if (e.target.nodeName === "svg" || e.target.nodeName === "path") {
-      if (
-        e.target.id === "forewords" &&
-        svgButtons[1].className !== "disable-button"
-      ) {
-        if (pageButtons[0].page < totalPages - 5) {
-          setPageButtons("forewords")
-        } else {
-          for (let i = 0; i < 5; i++) {
-            pageButtons[4 - i].page = totalPages - i;
-            if (pageButtons[4 - i].page === page) {
-              pageButtons[4 - i].className = "current-page";
-            } else {
-              pageButtons[4 - i].className = "";
-            }
-          }
-        }
-      } else if (
-        e.target.id === "backwards" &&
-        svgButtons[0].className !== "disable-button"
-      ) {
-        if (pageButtons[0].page < totalPages - 3) {
-          setPageButtons("backwards")
-        }
-      }
-    }
-
-    setPageButtonsState(pageButtons);
-    setSvgButtonsState(setSVGButtons(svgButtons, pageButtons));
-  };
-
+  /**
+   * @description sets pagination buttons and svg buttons on update / mount
+   */
   useEffect(() => {
+    //create pagination button array
     let pageButtons = [
       {
         id: "page-button-0",
@@ -121,6 +74,7 @@ function Pages({ query, totalPages }) {
       },
     ];
 
+    //create svg button array
     let svgButtons = [
       {
         id: "backwards",
@@ -132,44 +86,153 @@ function Pages({ query, totalPages }) {
       },
     ];
 
+    //if there is less then 5 pages (show each page)
     if (totalPages <= 5) {
+      //loop through all buttons and set there page number
       for (let i = 0; i < 5; i++) {
         if (i < totalPages) {
           pageButtons[i].page = i + 1;
           pageButtons[i].className = "";
           if (i + 1 === page) {
+            //set current page class
             pageButtons[i].className = "current-page";
           }
         } else {
+          //if no page hide the button
           pageButtons[i].className = "hide-button";
         }
       }
     } else {
+      //if there is more then 5 pages and the current page is not in the top 4 pages
       if (page < totalPages - 4) {
-        for (let i = 0; i < 5; i++) {
-          let value;
-          if (i === 3) value = "...";
-          else if (i === 4) value = totalPages;
-          else value = page + i;
-          pageButtons[i].page = value;
-          if(i === 3) pageButtons[i].className = "no-hover";
-          else pageButtons[i].className = "";
+        //set second last button to ""..."" and remove hover state
+        pageButtons[3].page = "...";
+        pageButtons[3].className = "no-hover";
+        //set last button to the last page
+        pageButtons[4].page = totalPages;
+        //loop through remaining buttons setting the current page as item 0 and up from there
+        for (let i = 0; i < 3; i++) {
+          pageButtons[i].page = page + i;
+          pageButtons[i].className = "";
         }
+        //set button 0 as current page
         pageButtons[0].className = "current-page";
       } else {
+        //if there is more then 5 pages and the current page is in the top 4 pages
+        //loop through all buttons and set there page number going backwards from the last page
         for (let i = 0; i < 5; i++) {
           pageButtons[4 - i].page = totalPages - i;
           pageButtons[4 - i].className = "";
+          //set button of current page
           if (pageButtons[4 - i].page === page) {
             pageButtons[4 - i].className = "current-page";
           }
         }
       }
     }
+    //finally update state to be rendered to the page
     setPageButtonsState(pageButtons);
     setSvgButtonsState(setSVGButtons(svgButtons, pageButtons));
-  }, [setPageButtonsState, page, totalPages, setSvgButtonsState, setSVGButtons]);
+  }, [
+    setPageButtonsState,
+    page,
+    totalPages,
+    setSvgButtonsState,
+    setSVGButtons,
+  ]);
 
+  /**
+   * @description awaits for button container to be clicked and then sets state to update the component
+   * @param e = event object
+   */
+  const pageContainerClick = (e) => {
+    //create a deep copy of each array object
+    let pageButtons = JSON.parse(JSON.stringify(pageButtonsState));
+    let svgButtons = JSON.parse(JSON.stringify(svgButtonsState));
+
+    //set page number clicked (if button and not SVG click)
+    let location = parseInt(e.target.innerHTML);
+
+    /**
+     * @description sets the page buttons to be shown *in most cases
+     * @param direction = either forewords or backwards depending on which svg is clicked
+     */
+    const setPageButtons = (direction) => {
+      //set last button to last page
+      pageButtons[4].page = totalPages;
+      //set second last button to "..." and remove hover state
+      pageButtons[3].page = "...";
+      pageButtons[3].className = "no-hover";
+      //loop through remaining buttons setting the current page as item 0 and up from there
+      for (let i = 0; i < 3; i++) {
+        //if backwards change all buttons by -1 others change all buttons by +1
+        direction === "backwards"
+          ? (pageButtons[i].page = pageButtons[i].page - 1)
+          : (pageButtons[i].page = pageButtons[i].page + 1);
+        //set button of current page if visible
+        if (pageButtons[i].page === page) {
+          pageButtons[i].className = "current-page";
+        } else {
+          pageButtons[i].className = "";
+        }
+      }
+    };
+
+    //only run on button click that isn't the current page or "..."
+    if (
+      e.target.nodeName === "BUTTON" &&
+      e.target.innerHTML !== "..." &&
+      location !== page
+    ) {
+      //push new page into history and update components
+      query.page = location;
+      history.push({
+        pathname: "/",
+        search: queryString.stringify(query),
+      });
+    } else if (e.target.nodeName === "svg" || e.target.nodeName === "path") {
+      //else if svg or svg path click
+      if (
+        e.target.id === "forewords" &&
+        svgButtons[1].className !== "disable-button"
+      ) {
+        if (pageButtons[0].page < totalPages - 5) {
+          //if pages are not the top 5 pages run setPageButtons function
+          setPageButtons("forewords");
+        } else {
+          //loop through all buttons setting the last page as the last button and going backwards from there
+          for (let i = 0; i < 5; i++) {
+            pageButtons[4 - i].page = totalPages - i;
+            //set button of current page if visible
+            if (pageButtons[4 - i].page === page) {
+              pageButtons[4 - i].className = "current-page";
+            } else {
+              pageButtons[4 - i].className = "";
+            }
+          }
+        }
+      } else if (
+        e.target.id === "backwards" &&
+        svgButtons[0].className !== "disable-button"
+      ) {
+        if (pageButtons[0].page < totalPages - 3) {
+          //if pages are not the top 3 pages run setPageButtons function
+          setPageButtons("backwards");
+        }
+      }
+    }
+
+    //finally update state to update component
+    setPageButtonsState(pageButtons);
+    setSvgButtonsState(setSVGButtons(svgButtons, pageButtons));
+  };
+
+  /**
+   * @description creates SVG element
+   * @param className = any CSS classes to be added to the svg
+   * @param id = a ID & key to be added to the svg
+   * @return SVG
+   */
   const createSVG = (className, id) => (
     <svg
       className={className}
@@ -188,12 +251,14 @@ function Pages({ query, totalPages }) {
     </svg>
   );
 
+  //if there is only 1 page return (render)nothing
   if (pageButtonsState === null || totalPages <= 1) {
     return null;
   } else {
     return (
       <div onClick={(e) => pageContainerClick(e)} className="buttons-container">
         {createSVG(svgButtonsState[0].className, svgButtonsState[0].id)}
+        {/* Loop through buttons array and create a jsx element for each*/}
         {pageButtonsState.map((pageButton) => (
           <button className={pageButton.className} key={pageButton.id}>
             {pageButton.page}
